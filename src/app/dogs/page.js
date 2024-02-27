6919e996bcaceb1c1fcce8ccd7d7630d08256965
Sam Louloudis <samanthaleelouloudis@gmail.com>
2024-02-27 08:28:56 +0000

    add metadata

---
+++ src/app/dogs/page.js
@@ -0,0 +1,31 @@
+import Link from "next/link"
+export const metadata = {
+    title: 'Dogs!',
+    description: 'This is a page about dogs | Cats vs Dogs',
+    openGraph: {
+        title: 'Dogs!',
+        type: "website",
+        url: "",
+        image: "/catsvsdogs.jpg",
+    }
+}
+
+
+export default async function Dogs() {
+    const request = await fetch(`https://dogapi.dog/api/v2/breeds
+    `)
+    const dogsData = await request.json()
+    const dogs = dogsData.data
+
+    return (
+        <div>
+            <h2>Dogs are very cute. But they can be loud</h2>
+            {dogs.map(dog => (
+               <div key={dog.id}>
+                 <h2>{dog.attributes.name}</h2>
+                <Link href={`/dogs/${dog.id}`}>Read more</Link>
+               </div>
+            ))}
+        </div>
+    )
+}
